8ad84c28fc42b4bcbb7ebd4f94f72d25e1aa3a3b
Pranav Kaviraj Reddy <pranavreddy1721@gmail.com>
2025-12-09 07:55:19 +0530

    Update 3583.countspecialtriplets_day67.cpp

--- 3583.countspecialtriplets_day67.cpp
+++ 3583.countspecialtriplets_day67.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
+    // main implementation (kept as a named function for clarity)
     int countTriplets(vector<int>& nums) {
-        const long long MOD = 1e9 + 7;
+        const long long MOD = 1000000007LL;
         long long ans = 0;
         unordered_map<long long, long long> left, right;
 
         for (int x : nums) right[x]++;
 
-        for (int j = 0; j < nums.size(); j++) {
+        for (int j = 0; j < (int)nums.size(); ++j) {
             long long val = nums[j];
-            right[val]--;  // remove j from right side
+            right[val]--;  // remove current j from right
 
             long long target = val * 2;
             if (left.count(target) && right.count(target)) {
                 ans = (ans + (left[target] * right[target]) % MOD) % MOD;
             }
 
-            left[val]++;  // add j to left side
+            left[val]++;  // add current j to left
         }
-        return ans;
+        return (int)ans;
+    }
+
+    // wrapper with the name the judge is calling
+    int specialTriplets(vector<int>& nums) {
+        return countTriplets(nums);
     }
 };
