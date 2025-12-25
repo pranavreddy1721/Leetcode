b65627399c5913bc6bc2a25c5214085a9601f691
Pranav Kaviraj Reddy <pranavreddy1721@gmail.com>
2025-12-25 20:09:52 +0530

    Create 3075.maximizehappinessofselectedchildren_day83.cpp

---
+++ 3075.maximizehappinessofselectedchildren_day83.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end(), greater<int>());
+        
+        long long ans = 0;
+        for (int i = 0; i < k; i++) {
+            long long curr = happiness[i] - i;
+            if (curr > 0)
+                ans += curr;
+            else
+                break; // further values will be <= 0
+        }
+        return ans;
+    }
+};
