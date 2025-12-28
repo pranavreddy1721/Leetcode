26d265d38f2b06d2685372c8d26c5443377cc96f
Pranav Kaviraj Reddy <pranavreddy1721@gmail.com>
2025-12-28 12:45:24 +0530

    Create 1351.countnegaTivenumbersinasortedmatrix_day86.cpp

---
+++ 1351.countnegaTivenumbersinasortedmatrix_day86.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int r = 0, c = n - 1;
+        int count = 0;
+
+        while (r < m && c >= 0) {
+            if (grid[r][c] < 0) {
+                count += (m - r);
+                c--;
+            } else {
+                r++;
+            }
+        }
+        return count;
+    }
+};
