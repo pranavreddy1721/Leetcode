3e8e504dc8cffddd32c110afb3f3674722888d28
Pranav Kaviraj Reddy <pranavreddy1721@gmail.com>
2026-01-02 19:18:41 +0530

    Create 961.N-repeatedelementinsize2Narray_day91.cpp

---
+++ 961.N-repeatedelementinsize2Narray_day91.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (i + 1 < n && nums[i] == nums[i + 1])
+                return nums[i];
+            if (i + 2 < n && nums[i] == nums[i + 2])
+                return nums[i];
+            if (i + 3 < n && nums[i] == nums[i + 3])
+                return nums[i];
+        }
+        
+        return -1; // never reached due to problem constraints
+    }
+};
