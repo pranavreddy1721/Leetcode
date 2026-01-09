4aee63c9a901f6cd24681041548f9f5c0d674171
Pranav Kaviraj Reddy <pranavreddy1721@gmail.com>
2026-01-09 21:23:28 +0530

    Create 865.smallestsubtreewithallthedeepestnodes_day98.cpp

---
+++ 865.smallestsubtreewithallthedeepestnodes_day98.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    pair<TreeNode*, int> dfs(TreeNode* root) {
+        if (!root) return {nullptr, 0};
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        if (left.second == right.second) {
+            // Current node is LCA of deepest nodes
+            return {root, left.second + 1};
+        }
+        else if (left.second > right.second) {
+            return {left.first, left.second + 1};
+        }
+        else {
+            return {right.first, right.second + 1};
+        }
+    }
+
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        return dfs(root).first;
+    }
+};
